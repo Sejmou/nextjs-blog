493bee90190cc5d1463b48c209bcd978bf15e21f
Sejmou <samo.kolter@gmail.com>
2022-08-21 13:11:18 +0200

    add prisma client

---
+++ lib/prisma.ts
@@ -0,0 +1,17 @@
+// we'll use a single PrismaClient instance in our backend API
+// so, we create it here
+// this instance should be imported wherever needed
+import { PrismaClient } from '@prisma/client';
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
